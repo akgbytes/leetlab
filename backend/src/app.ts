811dc1e163b643145a1b61d968834145e8240deb
Aman Kumar Gupta <akgbytes@gmail.com>
2025-05-07 05:10:10 +0530

    feat: add Winston logger and Prettier configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Leetlab");
+app.get('/', (req, res) => {
+  res.send('Welcome to Leetlab');
 });
 
 export default app;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
-import app from "./app";
+import app from './app';
+import logger from './configs/logger';
 
 const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
