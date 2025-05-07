23803ae40cbe87d74e3715a3bf40099886f160bc
Aman Kumar Gupta <akgbytes@gmail.com>
2025-05-07 17:37:29 +0530

    feat: add cookie-parser, cors, and helmet middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,25 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import helmet from 'helmet';
 import { errorHandler } from './middlewares/error.middlewares';
 import healthRoutes from './routes/healthCheck.routes';
+import { env } from './configs/env';
 
 const app = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: env.CLIENT_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
+);
 
 app.get('/', (req, res) => {
   res.send('Welcome to Leetlab');
